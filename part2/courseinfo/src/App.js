c6670d70948f28cdc0c5d619a9f129ec392eb199
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-06-23 13:57:52 +0800

    2.1 courseinfo (b)

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -29,21 +29,24 @@ const Total = ({ parts }) => {
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
-      { name: 'Fundamental of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 },
+      {
+        name: 'Fundamental of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      { name: 'State of a component', exercises: 14, id: 3 },
     ],
   }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
