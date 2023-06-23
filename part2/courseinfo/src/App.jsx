ab68492223887916e0d79bd52ab3b9296800c67c
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-06-23 14:48:19 +0800

    2.5 courseinfo

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,42 +1,4 @@
-const Header = ({ course }) => {
-  return <h2>{course}</h2>
-}
-
-const Part = ({ part, exercises }) => (
-  <p>
-    {part} {exercises}
-  </p>
-)
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map(part => (
-        <Part key={part.id} part={part.name} exercises={part.exercises} />
-      ))}
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, p) => s + p.exercises, 0)
-
-  return (
-    <p>
-      <strong>total of {total} exercises</strong>
-    </p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
