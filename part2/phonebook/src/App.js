486c7f6f69c284a573200405133041c640cfda93
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-06-24 19:41:11 +0800

    2.6 phonebook (f)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,18 +8,39 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const handleChange = e => {
+    setNewName(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const newPerson = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {persons.map(p => (
+        <>
+          {p.name}
+          <br />
+        </>
+      ))}
     </div>
   )
 }
