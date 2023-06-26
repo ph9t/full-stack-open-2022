397fbed4fbfc8dd4e8f6610309135b63887a482d
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-06-27 05:37:54 +0800

    2.12 phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -54,9 +54,11 @@ const App = () => {
       number: newNumber,
     }
 
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
+    axios.post('http://localhost:3001/persons', newPerson).then(response => {
+      setPersons(persons.concat(response.data))
+      setNewName('')
+      setNewNumber('')
+    })
   }
 
   const filteredPersons = persons.filter(p =>
