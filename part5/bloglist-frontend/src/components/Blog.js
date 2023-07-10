f2dfbcafe7843da07b84855f0e461c039116897d
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-07-10 15:50:47 +0800

    5.7 bloglist

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showFull, setShowFull] = useState(false)
+
+  console.log(blog)
+
+  const borderStyle = {
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderRadius: 5,
+    margin: 3,
+    padding: 5,
+  }
+
+  const showFullStyle = { display: showFull ? '' : 'none' }
+
+  return (
+    <div style={borderStyle}>
+      {blog.title} {blog.author}{' '}
+      <button onClick={() => setShowFull(!showFull)}>
+        {showFull ? 'hide' : 'view'}
+      </button>
+      <div style={showFullStyle}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.user.name || blog.user.username}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
